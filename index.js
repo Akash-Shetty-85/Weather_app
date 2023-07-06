8e69c1ac80851ecf833f7854fc790332520fc961
Akash <mr.shetty28502@gmail.com>
2023-07-06 06:47:30 +0530

    upated UI

--- index.js
+++ index.js
@@ -7,17 +7,12 @@ const url = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}
 
 
 async function getWhetherByLocation(city){
-//    const resp =await fetch(url(city),{
-//     origin:"cros"
-//    });
-//    console.log(resp);
-//    const resoData = await resp.json();
-//    console.log(resoData);
-//    addWhetherToPage(resoData);
     
 try {
     const resp = await fetch(url(city), { origin: "cros" });
     const resoData = await resp.json();
+    console.log(resoData);
+    console.log(resoData.weather[0].description);
     addWhetherToPage(resoData);
   } catch (error) {
     search.value='';
@@ -27,7 +22,6 @@ try {
   }
 }
 
-// console.log(getWhetherByLocation("india"));
 
 const KToC=(K)=>{
     return Math.floor(K-273.15);
@@ -50,15 +44,23 @@ function addWhetherToPage(Data){
    const temp = KToC(Data.main.temp)
 
     const weather = document.createElement("div");
-    weather.classList.add('weather');
+    weather.classList.add('card');
     weather.innerHTML=`
-    <h1>${Data.name}<h1>
-    <h2>
-    <img src="https://openweathermap.org/img/wn/${Data.weather[0].icon}@2x.png"/>
-    ${temp}
-    <img src="https://openweathermap.org/img/wn/${Data.weather[0].icon}@2x.png"/>
-    </h2>
-    <small>${Data.weather[0].main}</small>
+    <h4>${Data.name}</h4>
+    <h6>${Data.weather[0].description}</h6>
+    <img src="https://openweathermap.org/img/wn/${Data.weather[0].icon}@2x.png" alt="${Data.weather[0].main}"/>
+    <h1>${temp}<sup>&deg;</sup>C</h1>
+    <div class ="container">
+        <div class='details'>
+        <p>max</p>
+        <span>${KToC(Data.main.temp_max)}<sup>&deg;</sup>C</span>
+        </div>
+        <div class="details">
+        <p>min</p>
+        <span>${KToC(Data.main.temp_min)}<sup>&deg;</sup>C</span>
+        </div>
+    </div>
+    
     `
     main.innerHTML="";
     main.appendChild(weather);
