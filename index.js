b1f30e21edcf93955087a3fd817da92c2b6d0b6f
Akash <mr.shetty28502@gmail.com>
2023-07-06 09:59:12 +0530

    updated ui with two wheather card

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ async function getWhetherByLocation(city){
 try {
     const resp = await fetch(url(city), { origin: "cros" });
     const resoData = await resp.json();
-    console.log(resoData);
-    console.log(resoData.weather[0].description);
+    
+    
     addWhetherToPage(resoData);
+    addWhetherToPage2(resoData)
   } catch (error) {
     search.value='';
     console.log(error);
@@ -46,18 +47,18 @@ function addWhetherToPage(Data){
     const weather = document.createElement("div");
     weather.classList.add('card');
     weather.innerHTML=`
-    <h4>${Data.name}</h4>
-    <h6>${Data.weather[0].description}</h6>
-    <img src="https://openweathermap.org/img/wn/${Data.weather[0].icon}@2x.png" alt="${Data.weather[0].main}"/>
-    <h1>${temp}<sup>&deg;</sup>C</h1>
-    <div class ="container">
+    <h4 class="card-title">${Data.name}</h4>
+    <h6 class="card-description">${Data.weather[0].description}</h6>
+    <img src="https://openweathermap.org/img/wn/${Data.weather[0].icon}@2x.png" alt="${Data.weather[0].main}" class="card-image"/>
+    <h1 class="card-temperature">${temp}<sup>&deg;</sup>C</h1>
+    <div class ="card-container">
         <div class='details'>
-        <p>max</p>
-        <span>${KToC(Data.main.temp_max)}<sup>&deg;</sup>C</span>
+        <p class="card-min&max">Max</p>
+        <span class="card-span">${KToC(Data.main.temp_max)}<sup>&deg;</sup>C</span>
         </div>
-        <div class="details">
-        <p>min</p>
-        <span>${KToC(Data.main.temp_min)}<sup>&deg;</sup>C</span>
+        <div class="card-details">
+        <p class="card-min&max">Min</p>
+        <span class="card-span">${KToC(Data.main.temp_min)}<sup>&deg;</sup>C</span>
         </div>
     </div>
     
@@ -68,3 +69,132 @@ function addWhetherToPage(Data){
     search.value='';
 
 };
+const input2 = document.getElementById("form1");
+const searchField =document.getElementById("search-focus");
+input2.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    const cityCard =searchField.value;    
+    try {
+        if (cityCard) {
+            addWhetherToPage2(cityCard);
+        }
+    } catch (error) {
+        alert("Please enter a valid city name.");
+    }
+})
+function addWhetherToPage2(data){
+console.log(data);
+const cardWeather = document.getElementById("card2")
+cardWeather.innerHTML=`
+<div class="container py-5">
+          
+              <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-md-9 col-lg-7 col-xl-5">
+          
+                  <div id="wrapper-bg" class="card text-white bg-image shadow-4-strong"
+                    style="background-image: url('https://giffiles.alphacoders.com/105/105296.gif')">
+                    <!-- Main current data -->
+                    <div class="card-header p-4 border-0">
+                      <div class="text-center mb-3">
+                        <p class="h2 mb-1" id="wrapper-name"></p>
+                        <p class="mb-1" id="wrapper-description"></p>
+                        <p class="display-1 mb-1" id="wrapper-temp"></p>
+                        <span class="">Pressure: <span id="wrapper-pressure"></span></span>
+                        <span class="mx-2">|</span>
+                        <span class="">Humidity: <span id="wrapper-humidity"></span></span>
+                      </div>
+                    </div>
+          
+                    <!-- Hourly forecast -->
+                    <div class="card-body p-4 border-top border-bottom mb-2">
+                      <div class="row text-center">
+                        <div class="col-2">
+                          <strong class="d-block mb-2">Now</strong>
+                          <img id="wrapper-icon-hour-now" src="" class="" alt="" />
+                          <strong class="d-block" id="wrapper-hour-now"></strong>
+                        </div>
+          
+                        <div class="col-2">
+                          <strong class="d-block mb-2" id="wrapper-time1"></strong>
+                          <img id="wrapper-icon-hour1" src="" class="" alt="" />
+                          <strong class="d-block" id="wrapper-hour1"></strong>
+                        </div>
+          
+                        <div class="col-2">
+                          <strong class="d-block mb-2" id="wrapper-time2"></strong>
+                          <img id="wrapper-icon-hour2" src="" class="" alt="" />
+                          <strong class="d-block" id="wrapper-hour2"></strong>
+                        </div>
+          
+                        <div class="col-2">
+                          <strong class="d-block mb-2" id="wrapper-time3"></strong>
+                          <img id="wrapper-icon-hour3" src="" class="" alt="" />
+                          <strong class="d-block" id="wrapper-hour3"></strong>
+                        </div>
+          
+                        <div class="col-2">
+                          <strong class="d-block mb-2" id="wrapper-time4"></strong>
+                          <img id="wrapper-icon-hour4" src="" class="" alt="" />
+                          <strong class="d-block" id="wrapper-hour4"></strong>
+                        </div>
+          
+                        <div class="col-2">
+                          <strong class="d-block mb-2" id="wrapper-time5"></strong>
+                          <img id="wrapper-icon-hour5" src="" class="" alt="" />
+                          <strong class="d-block" id="wrapper-hour5"></strong>
+                        </div>
+                      </div>
+                    </div>
+          
+                    <!-- Daily forecast -->
+                    <div class="card-body px-5">
+                      <div class="row align-items-center">
+                        <div class="col-lg-6">
+                          <strong>Today</strong>
+                        </div>
+          
+                        <div class="col-lg-2 text-center">
+                          <img id="wrapper-icon-today" src="" class="w-100" alt="" />
+                        </div>
+          
+                        <div class="col-lg-4 text-end">
+                          <span id="wrapper-forecast-temp-today"></span>
+                        </div>
+                      </div>
+          
+                      <div class="row align-items-center">
+                        <div class="col-lg-6">
+                          <strong>Tomorrow</strong>
+                        </div>
+          
+                        <div class="col-lg-2 text-center">
+                          <img id="wrapper-icon-tomorrow" src="" class="w-100" alt="" />
+                        </div>
+          
+                        <div class="col-lg-4 text-end">
+                          <span id="wrapper-forecast-temp-tomorrow">28</span>
+                        </div>
+                      </div>
+          
+                      <div class="row align-items-center">
+                        <div class="col-lg-6">
+                          <strong>Day after tomorrow</strong>
+                        </div>
+          
+                        <div class="col-lg-2 text-center">
+                          <img id="wrapper-icon-dAT" src="" class="w-100" alt="" />
+                        </div>
+          
+                        <div class="col-lg-4 text-end">
+                          <span id="wrapper-forecast-temp-dAT">28</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+          
+                </div>
+              </div>
+          
+            </div>
+`
+}
